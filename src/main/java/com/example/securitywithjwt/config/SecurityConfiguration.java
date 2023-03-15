fe32cbd21cd4a6cba7517171984283c352bd7833
farhan00hm <farhan96.hm@gmail.com>
2023-03-15 17:10:28 +0600

    fix the bug

--- src/main/java/com/example/securitywithjwt/config/SecurityConfiguration.java
+++ src/main/java/com/example/securitywithjwt/config/SecurityConfiguration.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
-    private final Filter jwtAuthFilter;
+    private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
 
     @Bean 
